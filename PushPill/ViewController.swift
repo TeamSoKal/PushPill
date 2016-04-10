b024ecd352106d26501bed6422b06ff488323ed8
Lee Sun-Hyoup <kciter@naver.com>
2016-04-10 10:24:30 +0900

    Initial Commit

---
+++ PushPill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PushPill
+//
+//  Created by LeeSunhyoup on 2016. 4. 10..
+//  Copyright © 2016년 Kim Ga-eun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
